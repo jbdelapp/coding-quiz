75f854f897e214fe66198d675475eed5da9b5c80
Joey Delapp <jbdelapp@gmail.com>
2020-06-28 19:38:36 -0500

    added .DS_Store to .gitignore file

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const questions = [
+    {
+        title: "Question 1: Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "Question 2: The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Question 3: The instructions for a function are enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "curly brackets"
+    },
+    {
+        title: "Question 4: A property of an object that is a function is called a ____.",
+        choices: ["method", "string", "stylesheet", "boolean"],
+        answer: "method"
+    },
+    {
+        title: "Question 5: The logical operator that represents 'or' is ____.",
+        choices: ["||", "OR", "&&", "==="],
+        answer: "||"
+    }
+];
+
 function initQuiz() {
     //  Initialize "time remaining" variable
         let timeRemaining=0;
@@ -191,14 +219,6 @@ function initQuiz() {
                 colEl2.append(parEl);
             }
         });
-    
-        
-    
-    
-    
-    
-    
-    
     }
     
 initQuiz();
